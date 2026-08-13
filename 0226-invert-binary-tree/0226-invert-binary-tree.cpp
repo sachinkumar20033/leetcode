b276da165f92d7e897c6b2c883e69955dd9b34bb
sachin kumar <sachinkr30122003@gmail.com>
2026-08-13 13:46:10 +0530

    Time: 0 ms (100%), Space: 12.5 MB (26.82%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -17,11 +17,9 @@ public:
             return NULL;
         }
         swap(root->left,root->right);
-
         invertTree(root->left);
         invertTree(root->right);
 
         return root;
-
     }
 };
