bdd8cc8f40c5dd6be86389bbe2c694c2c2c0f867
sachin kumar <sachinkr30122003@gmail.com>
2026-09-08 15:04:23 +0530

    Time: 0 ms (100%), Space: 8.1 MB (17.44%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,38 @@
 class Solution {
 public:
-    int fib(int n) {
-        if(n==0)
-        {
-            return 0;
+int solveByRecursion(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    if(n==1)
+    {
+        return 1;
+    }
+    return solveByRecursion(n-1)+solveByRecursion(n-2);
+}
 
-        }
-        if(n==1)
-        {
-            return 1;
-        }
-        return fib(n-1)+fib(n-2);
+int solveByMemorization(int n,vector<int>&dp)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    if(n==1)
+    {
+        return 1;
+    }
+
+    if(dp[n]!=-1)
+    {
+        return dp[n];
+    }
+    dp[n]=solveByMemorization(n-1,dp)+solveByMemorization(n-2,dp);
+    return dp[n];
+}
+    int fib(int n) {
+        vector<int>dp(n+1,-1);
+        return solveByMemorization(n,dp);
     }
 };
